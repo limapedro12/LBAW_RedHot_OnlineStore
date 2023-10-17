a7bf8202e6eea505953a4f73c497b5876be422e0
Pedro Januário <up202108768@edu.fe.up.pt>
2023-10-17 11:25:10 +0100

    SQL schema concerns.

---
+++ create.sql
@@ -0,0 +1,124 @@
+CREATE SCHEMA lbaw2352;
+SET search_path TO lbaw2352;
+
+DROP TABLE IF EXISTS Utilizador CASCADE;
+DROP TABLE IF EXISTS Compra CASCADE;
+DROP TABLE IF EXISTS Transporte CASCADE;
+DROP TABLE IF EXISTS Devolucao CASCADE;
+DROP TABLE IF EXISTS Reembolso CASCADE;
+DROP TABLE IF EXISTS Notificacao CASCADE;
+DROP TABLE IF EXISTS Comentario CASCADE;
+DROP TABLE IF EXISTS Produto CASCADE;
+DROP TABLE IF EXISTS Administrador CASCADE;
+DROP TABLE IF EXISTS Grupo_Encomendas CASCADE;
+DROP TABLE IF EXISTS Notificacao_Compra CASCADE;
+DROP TABLE IF EXISTS Notificacao_Devolucao CASCADE;
+DROP TABLE IF EXISTS Notificacao_Reembolso CASCADE;
+DROP TABLE IF EXISTS Notificacao_Stock CASCADE;
+DROP TABLE IF EXISTS Portes CASCADE;
+DROP TABLE IF EXISTS ProdutoCompra CASCADE;
+
+CREATE TABLE Utilizador (
+	id SERIAL PRIMARY KEY,
+  	nome VARCHAR(256) NOT NULL,
+  	email VARCHAR(256) UNIQUE NOT NULL,
+	password VARCHAR(256) NOT NULL
+);
+
+CREATE TABLE Compra (
+	id SERIAL PRIMARY KEY,
+  	timestamp TIMESTAMP NOT NULL CHECK (timestamp <= now()),
+  	total FLOAT NOT NULL CHECK (total >= 0),
+  	id_utilizador INTEGER NOT NULL REFERENCES Utilizador (id) ON UPDATE CASCADE
+);
+
+CREATE TABLE Transporte (
+	id SERIAL PRIMARY KEY,
+  	tipo VARCHAR(256) NOT NULL,
+  	precoAtual FLOAT NOT NULL CHECK (precoAtual >= 0),
+  	id_compra INTEGER NOT NULL REFERENCES Compra (ID) ON UPDATE CASCADE
+);
+
+CREATE TABLE Devolucao (
+	id SERIAL PRIMARY KEY,
+  	timestamp TIMESTAMP NOT NULL CHECK (timestamp <= now()),
+  	estado VARCHAR(256)
+);
+
+CREATE TABLE Reembolso (
+	id SERIAL PRIMARY KEY,
+  	timestamp TIMESTAMP NOT NULL CHECK (timestamp <= now()),
+  	estado VARCHAR(256)
+);
+
+CREATE TABLE Notificacao (
+	id SERIAL PRIMARY KEY,
+  	timestamp TIMESTAMP NOT NULL CHECK (timestamp <= now()),
+  	texto VARCHAR(256) NOT NULL
+);
+
+Create TABLE Administrador (
+	id SERIAL PRIMARY KEY,
+  	nome VARCHAR(256) NOT NULL
+);
+
+CREATE TABLE Produto (
+	id SERIAL PRIMARY KEY,
+  	nome VARCHAR(256) NOT NULL,
+  	precoAtual FLOAT NOT NULL CHECK (precoAtual >= 0),
+  	stock INTEGER NOT NULL CHECK (stock >= 0),
+  	id_administrador INTEGER REFERENCES Administrador (id) ON UPDATE CASCADE,
+  	id_utilizador INTEGER REFERENCES Utilizador (id) ON UPDATE CASCADE
+);
+
+CREATE TABLE Comentario (
+	id SERIAL PRIMARY KEY,
+  	timestamp TIMESTAMP NOT NULL CHECK (timestamp <= now()),
+  	texto TEXT NOT NULL,
+  	id_utilizador INTEGER NOT NULL REFERENCES Utilizador (id) ON UPDATE CASCADE,
+  	id_produto INTEGER NOT NULL REFERENCES Produto (id) ON UPDATE CASCADE
+);
+
+CREATE TABLE Grupo_Encomendas (
+	id SERIAL PRIMARY KEY,
+  	estado VARCHAR(256) NOT NULL,
+  	id_compra INTEGER NOT NULL REFERENCES Compra (id) ON UPDATE CASCADE,
+  	id_administrador INTEGER REFERENCES Administrador (id) ON UPDATE CASCADE
+);
+
+CREATE TABLE Notificacao_Compra (
+	id INTEGER PRIMARY KEY REFERENCES Notificacao (id) ON UPDATE CASCADE,
+  	id_compra INTEGER REFERENCES Compra (id) ON UPDATE CASCADE
+);
+
+CREATE TABLE Notificacao_Devolucao (
+	id INTEGER PRIMARY KEY REFERENCES Notificacao (id) ON UPDATE CASCADE,
+  	id_devolucao INTEGER REFERENCES Devolucao (id) ON UPDATE CASCADE
+);
+
+CREATE TABLE Notificacao_Reembolso (
+	id INTEGER PRIMARY KEY REFERENCES Notificacao (id) ON UPDATE CASCADE,
+  	id_reembolso INTEGER REFERENCES Reembolso (id) ON UPDATE CASCADE
+);
+
+CREATE TABLE Notificacao_Stock (
+	id INTEGER PRIMARY KEY REFERENCES Notificacao (id) ON UPDATE CASCADE,
+  	id_stock INTEGER REFERENCES Produto (id) ON UPDATE CASCADE,
+	id_administrador INTEGER REFERENCES Administrador (ID) ON UPDATE CASCADE
+);
+
+CREATE TABLE Portes (
+	id_compra INTEGER REFERENCES Compra (id) ON UPDATE CASCADE,
+  	id_transporte INTEGER REFERENCES Transporte (id) ON UPDATE CASCADE,
+ 	preco FLOAT NOT NULL CHECK (preco >= 0)
+);
+
+CREATE TABLE ProdutoCompra (
+  	preco FLOAT NOT NULL CHECK (preco >= 0),
+	id_compra INTEGER REFERENCES Compra (id) ON UPDATE CASCADE,
+  	id_produto INTEGER REFERENCES Produto (id) ON UPDATE CASCADE,
+ 	id_devolucao INTEGER REFERENCES Devolucao (id) ON UPDATE CASCADE,
+  	id_reembolso INTEGER REFERENCES Reembolso (id) ON UPDATE CASCADE
+);
+
+
---
+++ populate.sql
@@ -0,0 +1,1 @@
+SET search_path TO lbaw2352;
