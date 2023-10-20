b1e520c787be532290004f22fa74db129ebc17fc
Pedro Januário (up202108768) <psjvieira@gmail.com>
2023-10-20 16:06:55 +0100

    Update create.sql

--- create.sql
+++ create.sql
@@ -1,3 +1,4 @@
+DROP SCHEMA IF EXISTS lbaw2352;
 CREATE SCHEMA lbaw2352;
 SET search_path TO lbaw2352;
 
@@ -17,10 +18,6 @@ DROP TABLE IF EXISTS Notificacao_Reembolso CASCADE;
 DROP TABLE IF EXISTS Notificacao_Stock CASCADE;
 DROP TABLE IF EXISTS Portes CASCADE;
 DROP TABLE IF EXISTS ProdutoCompra CASCADE;
-DROP TABLE IF EXISTS ProdutoCarrinho CASCADE;
-DROP TABLE IF EXISTS ProdutoCompra CASCADE;
-DROP TABLE IF EXISTS ProdutoCompra CASCADE;
-
 
 CREATE TABLE Utilizador (
 	id SERIAL PRIMARY KEY,
@@ -33,26 +30,27 @@ CREATE TABLE Compra (
 	id SERIAL PRIMARY KEY,
   	timestamp TIMESTAMP NOT NULL CHECK (timestamp <= now()),
   	total FLOAT NOT NULL CHECK (total >= 0),
-  	id_utilizador INTEGER NOT NULL REFERENCES Utilizador (id) ON UPDATE CASCADE
+  	id_utilizador INTEGER NOT NULL REFERENCES Utilizador (id) ON UPDATE CASCADE,
 );
 
 CREATE TABLE Transporte (
 	id SERIAL PRIMARY KEY,
   	tipo VARCHAR(256) NOT NULL,
   	precoAtual FLOAT NOT NULL CHECK (precoAtual >= 0),
-  	id_compra INTEGER NOT NULL REFERENCES Compra (ID) ON UPDATE CASCADE
 );
 
 CREATE TABLE Devolucao (
 	id SERIAL PRIMARY KEY,
   	timestamp TIMESTAMP NOT NULL CHECK (timestamp <= now()),
-  	estado VARCHAR(256)
+  	estado VARCHAR(256),
+        id_compra INTEGER NOT NULL REFERENCES Compra (ID) ON UPDATE CASCADE
 );
 
 CREATE TABLE Reembolso (
 	id SERIAL PRIMARY KEY,
   	timestamp TIMESTAMP NOT NULL CHECK (timestamp <= now()),
-  	estado VARCHAR(256)
+  	estado VARCHAR(256),
+        id_compra INTEGER NOT NULL REFERENCES Compra (ID) ON UPDATE CASCADE
 );
 
 CREATE TABLE Notificacao (
@@ -75,7 +73,6 @@ CREATE TABLE Produto (
   	stock INTEGER NOT NULL CHECK (stock >= 0),
 	tsvectors TSVECTOR,
   	id_administrador INTEGER REFERENCES Administrador (id) ON UPDATE CASCADE,
-  	id_utilizador INTEGER REFERENCES Utilizador (id) ON UPDATE CASCADE ?????
 );
 
 CREATE TABLE Comentario (
@@ -133,15 +130,14 @@ CREATE TABLE Notificacao_Wishlist (
 CREATE TABLE Portes (
 	id_compra INTEGER REFERENCES Compra (id) ON UPDATE CASCADE,
   	id_transporte INTEGER REFERENCES Transporte (id) ON UPDATE CASCADE,
+        id_compra INTEGER NOT NULL UNIQUE REFERENCES Compra (ID) ON UPDATE CASCADE,
  	preco FLOAT NOT NULL CHECK (preco >= 0)
 );
 
-CREATE TABLE ProdutoCompra (
+CREATE TABLE Preco (
   	preco FLOAT NOT NULL CHECK (preco >= 0),
 	id_compra INTEGER REFERENCES Compra (id) ON UPDATE CASCADE,
   	id_produto INTEGER REFERENCES Produto (id) ON UPDATE CASCADE,
- 	id_devolucao INTEGER REFERENCES Devolucao (id) ON UPDATE CASCADE,
-  	id_reembolso INTEGER REFERENCES Reembolso (id) ON UPDATE CASCADE
 );
 
 CREATE TABLE ProdutoCarrinho (
