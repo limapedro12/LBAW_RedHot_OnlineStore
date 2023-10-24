ab7e2a6935d93a455fc7c535257182d393afbf68
Pedro Januário (up202108768) <psjvieira@gmail.com>
2023-10-24 09:39:51 +0100

    Start populate.sql

--- populate.sql
+++ populate.sql
@@ -1,1 +1,25 @@
 SET search_path TO lbaw2352;
+
+SET timezone = 'Europe/Lisbon';
+
+INSERT INTO Utilizador (id,nome,email,password) VALUES
+  (1,'Dominic Walter','et.eros@yahoo.couk','XRC92KGT3PX'),
+  (2,'Scott Mckenzie','nonummy.ultricies.ornare@icloud.org','HEE09TED7XV'),
+  (3,'Janna Santiago','proin.vel.nisl@yahoo.net','JLY18UFO0RY'),
+  (4,'Nathan Horne','dolor.nulla@icloud.couk','JFX71BSD7SC'),
+  (5,'Tiger Boyle','dignissim@hotmail.net','SDU69QXM4TH'),
+  (6,'James Gutierrez','dolor.quisque@hotmail.ca','GEV73ERP4BN'),
+  (7,'Miranda Holmes','curae.phasellus@protonmail.org','CQX21FNX0MB'),
+  (8,'Vincent Chambers','pede.nonummy@hotmail.edu','EVH63ZKS0DY'),
+  (9,'Inez Barrera','nibh.aliquam@outlook.ca','EJY18RGV2MU'),
+  (10,'Jordan Chandler','enim.nunc@icloud.edu','CKN22QHB2VH');
+  (11,'Octavia Hickman','enim.diam@protonmail.couk','FNQ14POC4VY'),
+  (12,'Neil Petersen','metus.aliquam@hotmail.net','VCC55JKP7GF'),
+  (13,'Kyla Tanner','tempor.lorem.eget@outlook.org','OME47XIN8YG'),
+  (14,'Leilani Sargent','amet.ornare.lectus@aol.org','OUQ44TKA3PO'),
+  (15,'Prescott Mayer','donec.dignissim@icloud.couk','DWF44MCK4DM'),
+  (16,'Dean Lawson','ut@aol.com','XJC01OKK3IO'),
+  (17,'John Holmes','nec.eleifend@protonmail.org','SPQ25VQX5BT'),
+  (18,'Colorado Lamb','sed.nec@yahoo.edu','UQY34WSJ5XT'),
+  (19,'Ralph Lambert','commodo.hendrerit@outlook.edu','JSA88KNH7BS'),
+  (20,'Brenna Cline','a.felis.ullamcorper@yahoo.org','RTF10SNH7CL');
