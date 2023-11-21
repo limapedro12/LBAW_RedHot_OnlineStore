56a722c69c60a15d8c0eec8c9b204788726b45d6
Pedro Jorge Mendes Jesus Landolt <up202103337@fe.up.pt>
2023-11-21 02:02:48 +0000

    Update file admin.blade.php

--- webapp/resources/views/pages/admin.blade.php
+++ webapp/resources/views/pages/admin.blade.php
@@ -1,11 +1,10 @@
 @extends('layouts.adminHeaderFooter')
 
 @section('content')
-<<<<<<< 6537656d576cc99e341cc4a1b000a8ffcafb9c79
+
+{{ Auth::guard('admin')->user()->nome }}
+
 <div class="adminContent">
-=======
-    {{ Auth::guard('admin')->user()->nome }}
->>>>>>> 16adcb487aaf964401f5d6d5ab52c8c004817c28
     <div class="adminPage">      
         <div class="adminSideBar">
             <div class="adminSearchBarOnSideBar">
