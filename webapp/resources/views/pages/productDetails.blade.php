f5755e490a716a6edc2c38cb6f45363b4722eaac
Pedro Landolt <up202103337@fe.up.pt>
2023-12-11 00:58:17 +0000

    TCW create all the new base for the product display

--- webapp/resources/views/pages/productDetails.blade.php
+++ webapp/resources/views/pages/productDetails.blade.php
@@ -14,9 +14,10 @@
                 <div class="productDetails">
                     <div class="productTitle">
                         <h2> {{ $product->nome }} </h2>
-                        @if(Auth::guard('admin')->check())
+                        @if (Auth::guard('admin')->check())
                             <div class="productEdit">
-                                <a href="{{ route('editProduct', ['id' => $product->id]) }}"><i class="fa-solid fa-edit"></i></a>
+                                <a href="{{ route('editProduct', ['id' => $product->id]) }}"><i
+                                        class="fa-solid fa-edit"></i></a>
                             </div>
                         @endif
                     </div>
@@ -50,102 +51,97 @@
                     </div>
                 </div>
 
-                <div class="productAddWishlist">
-                    
-                </div>
-                
+
 
             </div>
 
+            <div class="productBottomLeft">
+                <div class="delete">
+                    @if (Auth::guard('admin')->check())
+                        @csrf
+                        <input type="submit" id='editarProduto' value="Editar"
+                            action='/products/{{ $product->id }}/edit'>
+                        <input type="submit" id='alterarStockDoProduto' value="Alterar Stock"
+                            action='/products/{{ $product->id }}/changeStock'>
+                        <input type="submit" id='eliminarProduto' value="Eliminar"
+                            action='/products/{{ $product->id }}/delete'>
+                    @endif
+                </div>
+            </div>
+
+            <div class="productBottomRight">
+                <div class="productAddWishlist">
+                    @unless (Auth::guard('admin')->check())
+                        <?php
+                        $productWishlist = App\Models\Wishlist::where('id_utilizador', Auth::user()->id)
+                            ->where('id_produto', $product->id)
+                            ->first();
+                        ?>
+
+                        @if ($productWishlist == null)
+                            <div class="addToWishlist">
+                                @if (Auth::check())
+                                    <form method="POST"
+                                        action="{{ route('addToWishlist', ['id' => Auth::user()->id, 'id_product' => $product->id]) }}">
+                                        @csrf
+                                        <input type="submit" value="Adicionar à Wishlist">
+                                    </form>
+                                @endif
+                            </div>
+                        @else
+                            <div class="toWishlist">
+                                <a href="{{ route('listWishlist', ['id' => Auth::user()->id]) }}"><button>Já na Minha
+                                        Wishlist</button></a>
+                            </div>
+                        @endif
+                    @endunless
+                </div>
+
+                <div class="productPrices">
+                    @if ($product->desconto > 0)
+                        <div class="productOldPrice">
+                            <p class="discount">
+                                {{ $product->desconto * 100 }}%
+                            </p>
+                            <p class="oldPrice">
+                                {{ $product->precoatual }}
+                            </p>
+                            <p class="euro">€ </p>
+                        </div>
+                        <div class="productNewPrice">
+                            <p class="newPrice">
+                                {{ round($discountFunction($product->precoatual, $product->desconto), 2) }} €
+                            </p>
+                        </div>
+                    @else
+                        <div class="productPrice">
+                            <p class="Price">
+                                {{ $product->precoatual }}€
+                            </p>
+                        </div>
+                    @endif
+                </div>
 
 
+                <div class="productStock">
+                    <p> Stock: {{ $product->stock }} </p>
+                </div>
 
-
-        </div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        <img style="height: 200px;" src="{{ $product->getProductImage() }}" alt="Imagem do produto">
-        <section class="productDetails">
-            <h2> {{ $product->nome }} </h2>
-            <p>
-                @if ($product->desconto > 0)
-                    <span style = "text-decoration: line-through;">
-                        {{ $product->precoatual }}
-                    </span>&nbsp
-                @endif
-                {{ round($discountFunction($product->precoatual, $product->desconto), 2) }} €
-            </p>
-            @if ($product->desconto > 0)
-                <p> Desconto: {{ $product->desconto * 100 }}% </p>
-                <br>
-            @endif
-            <p> Categoria: {{ $product->categoria }} </p>
-            <p> Stock: {{ $product->stock }} </p>
-            <p> Descrição: </p>
-            <p> {{ $product->descricao }} </p>
-        </section>
-
-        @if ($product->stock > 0 && !Auth::guard('admin')->check())
-            <form action="/products/{{ $product->id }}/add_to_cart" method="POST">
-                @csrf
-                <label for="quantidade"> Quantidade: </label>
-                <input type="number" name="quantidade" id="quantidade" value="1" min="1"
-                    max="{{ $product->stock }}">
-                <button type="submit"> Adicionar ao carrinho </button>
-            </form>
-        @endif
-
-        @unless (Auth::guard('admin')->check())
-            <?php
-            $productWishlist = App\Models\Wishlist::where('id_utilizador', Auth::user()->id)
-                ->where('id_produto', $product->id)
-                ->first();
-            ?>
-
-            @if ($productWishlist == null)
-                <section class="addToWishlist">
-                    @if (Auth::check())
-                        <form method="POST"
-                            action="{{ route('addToWishlist', ['id' => Auth::user()->id, 'id_product' => $product->id]) }}">
+                <div class="productAddCart">
+                    @if ($product->stock > 0 && !Auth::guard('admin')->check())
+                        <form action="/products/{{ $product->id }}/add_to_cart" method="POST">
                             @csrf
-                            <input type="submit" value="Adicionar à Wishlist">
+                            <label for="quantidade"> Quantidade: </label>
+                            <input type="number" name="quantidade" id="quantidade" value="1" min="1"
+                                max="{{ $product->stock }}">
+                            <button type="submit"> Adicionar ao carrinho </button>
                         </form>
                     @endif
-                </section>
-            @else
-                <p>Este produto já se encontra na sua Wishlist</p>
-                <a href="{{ route('listWishlist', ['id' => Auth::user()->id]) }}"><button>Minha Wishlist</button></a>
-            @endif
-        @endunless
+                </div>
 
-        <section class="delete">
-            @if (Auth::guard('admin')->check())
-                @csrf
-                <input type="submit" id='editarProduto' value="Editar" action='/products/{{ $product->id }}/edit'>
-                <input type="submit" id='alterarStockDoProduto' value="Alterar Stock"
-                    action='/products/{{ $product->id }}/changeStock'>
-                <input type="submit" id='eliminarProduto' value="Eliminar" action='/products/{{ $product->id }}/delete'>
-            @endif
-        </section>
+            </div>
+
+        </div>
 
     </section>
 
